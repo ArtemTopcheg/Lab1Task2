60b8339fc3aa239df07d78734f8d2abafec4de3d
Артём Шустов < @DESKTOP-3G11VV9>
2022-09-26 21:24:33 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 2
+
+Console.WriteLine("Введите число N:");
+string n = Console.ReadLine();
+int N = Convert.ToInt32(n);
+string Elka = "*";
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine(Elka);
+    Elka += "*";
+}
